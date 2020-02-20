aec0930cdf959ae8f3aff73048b86f201c6b1965
Nolan Morley <Morley.58@osu.edu>
2020-02-19 19:06:49 -0500

    Authors

---
+++ src/Authors/Author.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Author.css";
+
+export default function Author() {
+    return (
+        <div className="Book">
+            <div className="lander">
+                <h1>Author</h1>
+                <p>Here are the authors.</p>
+            </div>
+        </div>
+    );
+}
