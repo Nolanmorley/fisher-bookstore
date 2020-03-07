94cba5236cda7e8eac6037580f9e9f8fce88e26e
Nolan Morley <Morley.58@osu.edu>
2020-03-07 17:37:45 -0500

    authors is done

--- src/Authors/Authors.js
+++ src/Authors/Authors.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Authors.css";
+import {AuthorDisplay} from "./AuthorDisplay.js";
 
-export default function Author() {
-    return (
-        <div className="Book">
-            <div className="lander">
-                <h1>Author</h1>
-                <p>Here are the authors.</p>
+export default class Authors extends Component{
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            authors: [
+                {
+                    author: "Nolan Morley"
+                },
+                {
+                    author: "J.K. Rowling"
+                }
+            ]
+        };
+    }
+    
+    render() {
+        return (
+            <div className="Authors">
+                <div className="lander">
+                    <AuthorDisplay authors={this.state.authors} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
